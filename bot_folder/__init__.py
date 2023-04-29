7156f1b74d1332bfddd6713a2be9af613cc91eb6
SERHII MELNYK <melser68@i.ua>
2023-04-29 22:31:44 +0200

    Update __init__.py

---
+++ bot_folder/__init__.py
@@ -0,0 +1,5 @@
+import export_func as basic
+import module_classes_2 as class_exp
+from intellect_input import recognize_command as neurone
+from clean_folder import main as clean
+import module_classes_2 as class_exp
