a834b06ac2c92bed55b08e965fe9d0200a8c25ea
Nevskiy911 <122637230+Nevskiy911@users.noreply.github.com>
2023-05-01 22:56:48 +0200

    Add files via upload

--- bot_folder/clean_folder.py
+++ bot_folder/clean_folder.py
@@ -4,10 +4,8 @@ import zipfile
 import os
 from pathlib import Path
 from unicodedata import normalize, is_normalized
-try:
-    import bot_folder.export_func as basic
-except ModuleNotFoundError:
-    import export_func as basic
+#import bot_folder.export_func as basic
+import export_func as basic
 import py7zr
 
 
